c5db0a82df072bfbe3750ae15e0b5ed396829480
AngeloCalabro <angelo.1991@hotmail.com>
2022-11-21 11:27:18 +0100

    scaffolding

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>hello vue!</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+@use './assets/style/partials/variables' as *;
+  h1{
+    color: $hokey-pokey;
+  }
+</style>
