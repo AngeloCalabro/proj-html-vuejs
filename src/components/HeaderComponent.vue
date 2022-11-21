fe7b13a95bf0d89d115c81ce13e1c20eb7176e47
AngeloCalabro <angelo.1991@hotmail.com>
2022-11-21 15:14:15 +0100

    seconda sezione

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -4,13 +4,13 @@
             <div class="navbar">
                 <img class="mx-2" src="../assets/images/gym_logo_1x.png" alt="logo avada gym">
                 <ul class="d-flex mx-2">
-                    <li class="text-uppercase p-2" v-for="(item, index) in store.linksNavTop" :key="index"><a href="#">{{ item.text }}</a></li>
+                    <li class="text-uppercase text-center p-2" v-for="(item, index) in store.linksNavTop" :key="index"><a href="#" :class="{'active': item.active}">{{ item.text }}</a></li>
                 </ul>
             </div>
-            <div class="jumbo">
+            <div class="jumbo container">
                 <h1 class="text-uppercase">NO PAIN NO GAIN</h1>
                 <p>Motivation is what gets you started. Habit is what keeps you going.</p>
-                <div class="btn text-uppercase p-2">Join Us Today</div>
+                <div class="my-btn text-uppercase p-2">Join Us Today</div>
             </div>
         </nav>
     </header>
@@ -37,22 +37,19 @@ import {store} from '../store';
 header{
     width: 100%;
     nav{
-        // width: 100%;
-        height: 600px;
+        min-height: 650px;
         max-width: 100%;
-        // max-height: 100%;
         position: relative;
         background-image: url('../assets/images/home-banner.jpg');
         background-position: center;
         background-repeat: no-repeat;
-        // background-attachment: fixed;
         background-size: cover;
         div.navbar{
             position: absolute;
             top: 0;
             right: 0;
             left: 0;
-            @include dflex('between');
+            @include dflex('around');
             img{
                 height: 100px;
             }
@@ -62,7 +59,8 @@ header{
                     a{
                         text-decoration: none;
                         color: $white;
-                        &:active{
+                        transition: 0.5s;
+                        &.active{
                             color: $chestnut-rose;
                         }
                         &:hover{
@@ -81,16 +79,20 @@ header{
                 h1 {
                     color: $white;
                     font-size: $fs-xxl;
+                    // white-space: nowrap
                 }
                 p{
                     color: $white;
                 }
-                div.btn{
+                div.my-btn{
+                    width: max-content;
                     color: $edward;
                     border: 1px solid $edward;
+                    transition: 1s;
                     &:hover{
                         color: $white;
                         border: 1px solid $white;
+                        cursor: pointer;
                     }
                 }
 
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,16 +1,78 @@
 <template>
-    <div>
-        Main
-    </div>
+    <main>
+        <section class="container text-center p-5">
+        <h2><span class="text-uppercase">World class</span> Fitness</h2>
+        <div class="p-5"><img src="../assets/images/divider-xx-red.png" alt="divider"></div>
+        <p class="px-5">At Avada Gym, everything we do from to bottom is of the highest quality to ensure we're equipped to help you reach your goals of a healthy and fit lifestyle.</p>
+        </section>
+
+        <section id="second-section">
+            <div class="container py-5">
+                <div class="d-flex align-items-center">
+                    <div class="p-2">
+                        <img class="img-fluid" src="../assets/images/home_img-compressor.jpg" alt="">
+                    </div>
+                    <div>
+                    <h3 class="text-capitalize">Sign up for a free training session!</h3>
+                    <form @click.prevent="">
+                        <div class="mb-3">
+                            <label class="form-label">Name *</label>
+                            <input type="text" class="form-control">
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Email</label>
+                            <input type="email" class="form-control">
+                        </div>
+                        <div class="my-btn text-uppercase text-center p-2">Start Now!</div>
+                    </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="third-section">
+        
+        </section>
+    </main>
 </template>
 
 <script>
+import {store} from '../store'
     export default {
-        name: 'MainComponent'
+        name: 'MainComponent',
+        data(){
+            return{
+                store,
+            }
+        }
     }
 </script>
 
 <style lang="scss" scoped>
 @use '../assets/style/partials/variables' as *;
+@use '../assets/style/partials/mixins' as *;
 
+main{
+    #second-section{
+        background-color: $woodsmoke;
+
+        input{
+            background-color: $woodsmoke;
+        }
+        div.my-btn {
+                color: $white;
+                background-color: $vibrant;
+                border: 1px solid $vibrant;
+                transition: 1s;
+        
+                &:hover {
+                    color: $white;
+                    background-color: $chestnut-rose;
+                    border: 1px solid $chestnut-rose;
+                    cursor: pointer;
+                }
+            }
+    }
+
+}
 </style>
